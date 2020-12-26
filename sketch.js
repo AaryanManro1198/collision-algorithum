d7f47a0d83123366cd6c433b1494380bc770e07d
AaryanManro1198 <76572549+AaryanManro1198@users.noreply.github.com>
2020-12-26 14:36:53 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(800,400);
+
+  moving=createSprite(100, 200, 50, 50); 
+  fixed=createSprite(200, 100, 50, 50); 
+  moving.shapeColor="green"; 
+  fixed.shapeColor="green"; 
+  moving.debug=true; 
+  fixed.debug=true;
+
+}
+
+function draw() {
+  background(255,255,255);
+  
+  moving.x=mouseX; moving.y=mouseY; 
+  if(moving.x-fixed.x<moving.width/2+fixed.width/2&&fixed.x-moving.x<moving.width/2+fixed.width/2&&moving.y-fixed.y<moving.height/2+fixed.height/2&&fixed.y-moving.y<moving.height/2+fixed.height/2) { 
+    moving.shapeColor="red"; 
+    fixed.shapeColor="red"; 
+  }
+  else{ 
+    moving.shapeColor="green"; 
+    fixed.shapeColor="green";
+  }
+
+  drawSprites();
+}
